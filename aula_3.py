4eeb172f7664e4c0e779f665dc035f84ac3a121e
adrianohenriquess <adriano.henriquesds@gmail.com>
2023-10-24 21:38:20 -0300

    Tranformando a classe conta em abstrata e usando o decorator @abtractmethod

--- aula_3.py
+++ aula_3.py
@@ -1,18 +1,21 @@
 import array as ar
+from abc import abstractmethod, ABCMeta
+
 import numpy as np
-class Conta:
+class Conta(metaclass=ABCMeta):
     def __init__(self, codigo):
         self._codigo = codigo
         self._saldo = 0
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
 
     def deposita(self, valor):
         self._saldo += valor
 
     def __str__(self):
         return "[>>Codigo {} Saldo {} <<]".format(self._codigo, self._saldo)
-
-
-#print(Conta(88))
 
 class ContaCorrente(Conta):
 
@@ -25,6 +28,13 @@ class ContaPoupanca(Conta):
         self._saldo *= 1.01
         self._saldo -= 3
 
+class ContaInvestimento(Conta):
+
+    def passa_o_mes(self):
+        self._saldo *= 1.06
+        self._saldo -= 1
+
+
 conta16 = ContaCorrente(16)
 conta16.deposita(1000)
 conta16.passa_o_mes()
@@ -35,8 +45,12 @@ conta17.deposita(1000)
 conta17.passa_o_mes()
 print(conta17)
 
+conta18 = ContaInvestimento(18)
+conta18.deposita(450)
+conta18.passa_o_mes()
+
 #lista de contas
-contas = [conta16, conta17]
+contas = [conta16, conta17, conta18]
 for conta in contas:
     conta.passa_o_mes()
     print(conta)
